45fa060fa249db3caa49ccd2232c9d329460ad0c
Jean-Baptiste Terrazzoni <contact@jterrazz.com>
2025-12-13 11:54:07 +0100

    chore: add empty lines to card article components for improved readability

--- src/presentation/ui/molecules/card-article/card-article-compact.tsx
+++ src/presentation/ui/molecules/card-article/card-article-compact.tsx
@@ -43,3 +43,4 @@ export const CardArticleCompact: React.FC<CardArticleCompactProps> = ({
     );
 };
 
+
--- src/presentation/ui/molecules/card-article/card-article-horizontal.tsx
+++ src/presentation/ui/molecules/card-article/card-article-horizontal.tsx
@@ -92,3 +92,4 @@ export const CardArticleHorizontal: React.FC<CardArticleHorizontalProps> = ({
     );
 };
 
+
